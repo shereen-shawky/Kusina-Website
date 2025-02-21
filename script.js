fe7271d875a9f8b1f009c6973c787575b852df46
shereen-shawky <116681637+shereen-shawky@users.noreply.github.com>
2025-02-21 23:58:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.querySelector(".navbar");
+
+    if (navbar) {
+        window.addEventListener("scroll", () => {
+        if (window.scrollY > 50) {
+            navbar.style.backgroundColor = "rgba(255, 255, 255, 1)"; // Solid white background
+            navbar.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.1)";
+        } else {
+            navbar.style.backgroundColor = "transparent";
+            navbar.style.boxShadow = "none";
+        }
+        });
+    }
+    });
+    document.addEventListener("DOMContentLoaded", function () {
+        const rows = document.querySelectorAll(".scroll-item");
+
+        function handleScroll() {
+            rows.forEach((row, index) => {
+                const rect = row.getBoundingClientRect();
+                if (rect.top < window.innerHeight - 100) {
+                    setTimeout(() => {
+                        row.classList.add("show");
+                    }, index * 100); 
+                }
+            });
+        }
+
+        window.addEventListener("scroll", handleScroll);
+        handleScroll(); 
+    });
